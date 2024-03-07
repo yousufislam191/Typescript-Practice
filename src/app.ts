3d186ed9cc6ac99d107c84e4e1191683da39d3c8
yousufislam191 <yousufislam337@gmail.com>
2024-03-07 13:01:14 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express,{Application, Request, Response} from "express";
+import cors from "cors";
 
 const app: Application = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("server home route updated again.");
+  res.status(200).send("server home route updated.");
 });
 
 export { app };
